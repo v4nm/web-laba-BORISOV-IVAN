5f6e0edea1df1a71ef4de97744b19c422f0f32f0
borva <borva@DESKTOP-LENHML8>
2025-04-17 18:28:46 +0900

    upgrade

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ document.getElementById('toggelButton').addEventListener("click", function(){
     var extraImage = document.getElementById('extraImage');
     if (extraImage.style.display === 'none'){
         extraImage.style.display = 'block';
-        this.textContent = 'Скрыть';
+        this.textContent = 'Hide';
     } else {
         extraImage.style.display = 'none';
-        this.textContent = 'Показать еще';
+        this.textContent = 'Open';
     }
 })
